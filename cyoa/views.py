8f6f875fbce9a63659d9f9c984719992119409d9
jchaffee-intific <jchaffee@intific.org>
2019-02-08 15:56:11 -0600

    added socketio connections

--- cyoa/views.py
+++ cyoa/views.py
@@ -1,7 +1,14 @@
-from flask import render_template, abort
+import cgi
+from flask import render_template, abort, request
 from jinja2 import TemplateNotFound
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
 
-from . import app
+from .config import TWILIO_NUMBER
+from . import app, redis_db
+from . import socketio
+
+client = Client()
 
 @app.route('/<presentation_name>/', methods=['GET'])
 def landing(presentation_name):
@@ -9,3 +16,18 @@ def landing(presentation_name):
         return render_template(presentation_name + '.html')
     except TemplateNotFound:
         abort(404)
+
+@ap.route('/cyoa/twilio/webhook/', methods=['POST'])
+def twilio_callback():
+    to = request.form.get('To', '')
+    from_ = request.form.get('From', '')
+    message = reqest.form.get('Body', '').lower()
+    if to == TWILIO_NUMBER:
+        redis_db.incr(cgi.escape(message))
+        socketio.emit('msg', {'div': cgi.escape(message),
+                                'val': redis_db.get(message)},
+                        namespace='/cyoa')
+                        
+    resp = MessagingResponse()
+    resp.message("Thanks for your vote!")
+    return str(resp)
